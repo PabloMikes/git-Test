ef9b4fa5dacfdf7dbfd7f7a8b8225a1060f5eee0
PabloMikes <sefpaja@seznam.cz>
2023-12-20 11:16:16 +0100

    Task 2 - implemented second power of number.

---
+++ src/main/java/cz/spsmb/pm/gitTest/MathUtils.java
@@ -0,0 +1,15 @@
+package cz.spsmb.pm.gitTest;
+
+public class MathUtils {
+
+    /**
+     * second power of number
+     * @param number
+     * @return
+     */
+
+
+    static public final int secondPower(int number){
+        return number*number;
+    }
+}
