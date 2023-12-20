71f47f96a1e730205313f8049d00c976d549c5e8
PabloMikes <sefpaja@seznam.cz>
2023-12-20 11:33:03 +0100

    hail

--- src/main/java/cz/spsmb/pm/gitTest/Main.java
+++ src/main/java/cz/spsmb/pm/gitTest/Main.java
@@ -2,6 +2,6 @@ package cz.spsmb.pm.gitTest;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello world.");
     }
 }
